fb205cf4c7bdd9bbb6dc69c4ed0ee4a9646936e5
Karmvir <jadejakarmvir12@gmail.com>
2024-04-07 17:43:36 +0530

    Here Calculated and display the score achieved

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ import fs from "fs";
 import env from "dotenv";
 env.config();
 const app = express();
+let questions = []; // Define an empty array to store questions
+let Corr_Ans=[];
 
-// Declare questions as a let variable to allow reassignment
-let questions = [
-  
-];
 try {
+  // Read questions from the JSON file
   let data = fs.readFileSync('question.json', 'utf-8');
   questions = JSON.parse(data);
-  console.log(questions);
+  Corr_Ans= questions.map(question => question.answer);
+  console.log(Corr_Ans);
+
 } catch (err) {
   console.error('Error reading questions file:', err);
 }
@@ -22,16 +23,31 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.get('/', (req, res) => {
-  res.render('index', { questions
-  
-  });
+  res.render('index', { questions });
 });
 
+
 app.post('/submit', (req, res) => {
-  // Process the submitted answers here
-  console.log(req.body);
-  res.send('Answers submitted');
+let User_Ans=[];
+const submittedAnswers=req.body;
+
+Object.keys(submittedAnswers).forEach((questionIndex)=>{
+  const selectedOptionIndex =parseInt(submittedAnswers[questionIndex]);
+  User_Ans.push(selectedOptionIndex);
 });
+
+let score = 0;
+for (let i = 0; i < User_Ans.length; i++) {
+  if (User_Ans[i] === Corr_Ans[i]) {
+    score++;
+  }
+}
+
+res.render('result.ejs',{score,questions});
+
+});
+
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
